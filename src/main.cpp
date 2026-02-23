d4b08f53b051882778e60262dceb6fd26cc07b16
Schmidt Nandor <schmidt.nandor1@gmail.com>
2026-02-23 16:48:46 +0100

    sending kb input works now, experimenting with screen

--- src/main.cpp
+++ src/main.cpp
@@ -1,58 +1,47 @@
 #include <Arduino.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_ST7735.h>
-
-static constexpr uint8_t PIN_SWITCH = 21;
-static constexpr bool SWITCH_ACTIVE_LOW = true;
-static constexpr uint8_t TFT_CS = 17;
-static constexpr uint8_t TFT_DC = 16;
-static constexpr uint8_t TFT_RST = 20;
-static constexpr uint8_t TFT_SCK = 18;
-static constexpr uint8_t TFT_MOSI = 19;
-
-Adafruit_ST7735 tft(TFT_CS, TFT_DC, TFT_MOSI, TFT_SCK, TFT_RST);
-
+#include <Keyboard.h>
+Adafruit_ST7735 tft = Adafruit_ST7735(17,16,19,18,20);
+GFXcanvas1 canvas = GFXcanvas1(160,128);
+int testSwitchPin = 21;
 void setup() {
   Serial.begin(115200);
-  pinMode(PIN_SWITCH, SWITCH_ACTIVE_LOW ? INPUT_PULLUP : INPUT_PULLDOWN);
-
-  tft.initR(INITR_BLACKTAB);
+  Keyboard.begin(KeyboardLayout_en_US);
+  pinMode(testSwitchPin,INPUT_PULLUP);
+  tft.initR();
   tft.setRotation(1);
-  tft.fillScreen(ST77XX_BLACK);
-  tft.setTextColor(ST77XX_WHITE, ST77XX_BLACK);
-  tft.setTextSize(1);
-  tft.setCursor(0, 0);
-  tft.println("Ready");
-  tft.println("Waiting for switch...");
+  tft.setCursor(0,0);
+  tft.fillScreen(ST7735_BLACK);
+  canvas.setRotation(1);
+  canvas.setCursor(0,0);
+  canvas.fillRect(40, 16, 74, 62,1);
 }
 
-void loop() {
-  static int lastState = HIGH;
-  static unsigned long lastChangeMs = 0;
-  static unsigned long lastDrawMs = 0;
 
-  int state = digitalRead(PIN_SWITCH);
-  if (state != lastState && (millis() - lastChangeMs) > 20) {
-    lastChangeMs = millis();
-    lastState = state;
-    lastDrawMs = 0; // force redraw on change
+
+void loop(){
+  // tft.fillScreen(ST7735_BLACK);
+  // delay(1000);
+  // tft.drawRect(40, 16, 74, 62, 1);
+  // delay(1000);
+  // tft.drawBitmap(0, 0, pressedCanvas.getBuffer(),pressedCanvas.width(), pressedCanvas.height(), 0xFFFF, 0x0000);
+      
+if (digitalRead(testSwitchPin) == LOW)
+  {
+    Keyboard.press('A');
+    tft.setCursor(0,0);
+    tft.setTextColor(ST7735_WHITE, ST7735_BLACK);
+    tft.print("Switch pressed!");
+    Serial.println("Switch pressed!");
   }
-
-  if (millis() - lastDrawMs > 200) {
-    lastDrawMs = millis();
-    bool pressed = SWITCH_ACTIVE_LOW ? (state == LOW) : (state == HIGH);
-    Serial.println(pressed ? "GPIO21 pressed" : "GPIO21 released");
-
-   
-    tft.setCursor(0, 0);
-    tft.setTextColor(ST77XX_WHITE, ST77XX_BLACK);
-    tft.setTextSize(2);
-    tft.println(pressed ? "Pressed!" : "Released");
-    tft.setTextSize(1);
-    tft.println(" ");
-    tft.print("Raw: ");
-    tft.println(state == LOW ? "LOW" : "HIGH");
+  else 
+  {
+    Keyboard.release('A');
+    tft.setCursor(0,0);
+    tft.setTextColor(ST7735_WHITE, ST7735_BLACK);
+    tft.print("Switch not pressed!");
+    Serial.println("Switch not pressed!");
   }
-
-  delay(5);
 }
+
