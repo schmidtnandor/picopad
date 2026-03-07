5a8713bfaff986b61e9a7a1013ae9943d7dbbc36
Schmidt Nandor <schmidt.nandor1@gmail.com>
2026-03-07 12:25:01 +0100

    pressing keys now shows on tft

--- src/main.cpp
+++ src/main.cpp
@@ -3,85 +3,178 @@
 #include <Adafruit_ST7735.h>
 #include <Keyboard.h>
 Adafruit_ST7735 tft = Adafruit_ST7735(17, 16, 19, 18, 20);
-GFXcanvas1 canvas = GFXcanvas1(160, 128);
-int testSwitchPin = 21;
-int testSwitchPin2 = 10;
-bool isFilledRect = false;
-bool isFilledRect2 = false;
+int pins[9] = {0, 1, 2, 3, 4, 5, 6, 7, 8};
+const int keyCount = sizeof(pins) / sizeof(pins[0]);
+class Key {
+public:
+  int pin;
+  bool isFilledRect;
+};
+Key keys[9];
 void setup() {
   Serial.begin(115200);
   Keyboard.begin(KeyboardLayout_en_US);
-  pinMode(testSwitchPin, INPUT_PULLUP);
-  pinMode(testSwitchPin2, INPUT_PULLUP);
+  for (int pin : pins) {
+    pinMode(pin, INPUT_PULLUP);
+  }
+  for (int i = 0; i < keyCount; i++) {
+    keys[i].pin = pins[i];
+    keys[i].isFilledRect = false;
+  }
   tft.initR();
   tft.setRotation(1);
   tft.setCursor(0, 0);
   tft.fillScreen(ST7735_BLACK);
   tft.setTextColor(ST7735_WHITE, ST7735_BLACK);
-  canvas.setRotation(1);
-  canvas.setCursor(0, 0);
-  canvas.fillRect(9, 9, 13, 14, 1);
-  tft.drawRect(24, 8, 32, 32, 0xFFFF);
-
-  tft.drawRect(64, 8, 32, 32, 0xFFFF);
-
-  tft.drawRect(103, 8, 32, 32, 0xFFFF);
-
-  tft.drawRect(24, 48, 32, 32, 0xFFFF);
-
-  tft.drawRect(64, 48, 32, 32, 0xFFFF);
-
-  tft.drawRect(103, 48, 32, 32, 0xFFFF);
-
-  tft.drawRect(24, 87, 32, 32, 0xFFFF);
-
-  tft.drawRect(64, 87, 32, 32, 0xFFFF);
-
-  tft.drawRect(103, 87, 32, 32, 0xFFFF);
-  ;
+  tft.drawRect(8, 8, 32, 32, 0xFFFF);
+  tft.drawRect(8, 48, 32, 32, 0xFFFF);
+  tft.drawRect(8, 88, 32, 32, 0xFFFF);
+  tft.drawRect(48, 8, 32, 32, 0xFFFF);
+  tft.drawRect(48, 48, 32, 32, 0xFFFF);
+  tft.drawRect(48, 88, 32, 32, 0xFFFF);
+  tft.drawRect(88, 8, 32, 32, 0xFFFF);
+  tft.drawRect(88, 48, 32, 32, 0xFFFF);
+  tft.drawRect(88, 88, 32, 32, 0xFFFF);
+  tft.drawRect(128, 8, 24, 112, 0xFFFF);
 }
-
 void loop() {
 
-  if (digitalRead(testSwitchPin) == LOW) {
-    if (!isFilledRect) {
-      tft.fillRect(24, 8, 32, 32, 0xFFFF);
-      isFilledRect = true;
-    }
-
-    Keyboard.press('A');
-    tft.setCursor(0, 0);
-    Serial.println("Switch pressed!");
-    delay(50);
-  } else {
-    if (isFilledRect) {
-      tft.fillRect(24, 8, 32, 32, 0x0000);
-      tft.drawRect(24, 8, 32, 32, 0xFFFF);
-    }
-
-    isFilledRect = false;
-    Keyboard.release('A');
-    tft.setCursor(0, 0);
-    tft.setTextColor(ST7735_WHITE, ST7735_BLACK);
-    Serial.println("Switch not pressed!");
-    delay(50);
-  }
-  // ezzel még valami van + borzasztóan fog kinézni a kód xd
-  if (digitalRead(testSwitchPin2) == LOW) {
-    if (!isFilledRect2) {
-      tft.fillRect(64, 8, 32, 32, 0xFFFF);
-      isFilledRect2 = true;
-    } else {
-      if (isFilledRect2) {
-        tft.fillRect(64, 8, 32, 32, 0x0000);
-        tft.drawRect(64, 8, 32, 32, 0xFFFF);
+  for (int pin : pins) {
+    if (digitalRead(pin) == LOW) {
+      switch (pin) {
+      case 0:
+        Keyboard.write('a');
+        if (keys[0].isFilledRect == false) {
+          tft.fillRect(8, 8, 32, 32, 0xFFFF);
+          keys[0].isFilledRect = true;
+        }
+        break;
+      case 1:
+        Keyboard.write('b');
+        if (keys[1].isFilledRect == false) {
+          tft.fillRect(8, 48, 32, 32, 0xFFFF);
+          keys[1].isFilledRect = true;
+        }
+        break;
+      case 2:
+        Keyboard.write('c');
+        if (keys[2].isFilledRect == false) {
+          tft.fillRect(8, 88, 32, 32, 0xFFFF);
+          keys[2].isFilledRect = true;
+        }
+        break;
+      case 3:
+        Keyboard.write('d');
+        if (keys[3].isFilledRect == false) {
+          tft.fillRect(48, 8, 32, 32, 0xFFFF);
+          keys[3].isFilledRect = true;
+        }
+        break;
+      case 4:
+        Keyboard.write('e');
+        if (keys[4].isFilledRect == false) {
+          tft.fillRect(48, 48, 32, 32, 0xFFFF);
+          keys[4].isFilledRect = true;
+        }
+        break;
+      case 5:
+        Keyboard.write('f');
+        if (keys[5].isFilledRect == false) {
+          tft.fillRect(48, 88, 32, 32, 0xFFFF);
+          keys[5].isFilledRect = true;
+        }
+        break;
+      case 6:
+        Keyboard.write('g');
+        if (keys[6].isFilledRect == false) {
+          tft.fillRect(88, 8, 32, 32, 0xFFFF);
+          keys[6].isFilledRect = true;
+        }
+        break;
+      case 7:
+        Keyboard.write('h');
+        if (keys[7].isFilledRect == false) {
+          tft.fillRect(88, 48, 32, 32, 0xFFFF);
+          keys[7].isFilledRect = true;
+        }
+        break;
+      case 8:
+        Keyboard.write('i');
+        if (keys[8].isFilledRect == false) {
+          tft.fillRect(88, 88, 32, 32, 0xFFFF);
+          keys[8].isFilledRect = true;
+        }
+        break;
       }
+    } else {
+      switch (pin) {
+      case 0:
+        if (keys[0].isFilledRect == true) {
+          tft.fillRect(8, 8, 32, 32, 0x0000);
+          tft.drawRect(8, 8, 32, 32, 0xFFFF);
+        }
+        keys[0].isFilledRect = false;
+        break;
+      case 1:
+        if (keys[1].isFilledRect == true) {
+          tft.fillRect(8, 48, 32, 32, 0x0000);
+          tft.drawRect(8, 48, 32, 32, 0xFFFF);
+        }
+        keys[1].isFilledRect = false;
+        break;
+      case 2:
+        if (keys[2].isFilledRect == true) {
+          tft.fillRect(8, 88, 32, 32, 0x0000);
+          tft.drawRect(8, 88, 32, 32, 0xFFFF);
+        }
+        keys[2].isFilledRect = false;
+        break;
+      case 3:
+        if (keys[3].isFilledRect == true) {
+          tft.fillRect(48, 8, 32, 32, 0x0000);
+          tft.drawRect(48, 8, 32, 32, 0xFFFF);
+        }
+        keys[3].isFilledRect = false;
+        break;
+      case 4:
+        if (keys[4].isFilledRect == true) {
+          tft.fillRect(48, 48, 32, 32, 0x0000);
+          tft.drawRect(48, 48, 32, 32, 0xFFFF);
+        }
+        keys[4].isFilledRect = false;
+        break;
+      case 5:
+        if (keys[5].isFilledRect == true) {
+          tft.fillRect(48, 88, 32, 32, 0x0000);
+          tft.drawRect(48, 88, 32, 32, 0xFFFF);
+        }
+        keys[5].isFilledRect = false;
+        break;
+      case 6:
+        if (keys[6].isFilledRect == true) {
+          tft.fillRect(88, 8, 32, 32, 0x0000);
+          tft.drawRect(88, 8, 32, 32, 0xFFFF);
+        }
+        keys[6].isFilledRect = false;
+        break;
+      case 7:
+        if (keys[7].isFilledRect == true) {
+          tft.fillRect(88, 48, 32, 32, 0x0000);
+          tft.drawRect(88, 48, 32, 32, 0xFFFF);
+        }
+        keys[7].isFilledRect = false;
+        break;
+      case 8:
+        if (keys[8].isFilledRect == true) {
+          tft.fillRect(88, 88, 32, 32, 0x0000);
+          tft.drawRect(88, 88, 32, 32, 0xFFFF);
+        }
+        keys[8].isFilledRect = false;
+        break;
 
-      isFilledRect2 = false;
-      tft.setCursor(0, 0);
-      tft.setTextColor(ST7735_WHITE, ST7735_BLACK);
-      Serial.println("Switch not pressed!");
-      delay(50);
+      default:
+        break;
+      }
     }
   }
 }
