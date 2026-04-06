d1cc228c604b6d10c397adef22166e28851694b0
schmidtnandor <schmidt.nandor@students.jedlik.eu>
2026-04-06 20:20:26 +0200

    made code easier to read and configure, and removed useless code

--- code_with_pio_config/src/main.cpp
+++ code_with_pio_config/src/main.cpp
@@ -2,8 +2,23 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_ST7735.h>
 #include <Keyboard.h>
-Adafruit_ST7735 tft = Adafruit_ST7735(5, 6, 3, 2, 7);
-int pins[9] = {9, 12, 15, 10, 11, 19, 16, 21, 17};
+const int TFT_CS = 5;
+const int TFT_DC = 6;
+const int TFT_MOSI = 3;
+const int TFT_SCLK = 2;
+const int TFT_RST = 7;
+Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_MOSI, TFT_SCLK, TFT_RST);
+const int TOP_LEFT_PIN = 9;
+const int TOP_MIDDLE_PIN = 15;
+const int TOP_RIGHT_PIN = 16;
+const int MIDDLE_LEFT_PIN = 10;
+const int MIDDLE_MIDDLE_PIN = 11;
+const int MIDDLE_RIGHT_PIN = 21;
+const int BOTTOM_LEFT_PIN = 12;
+const int BOTTOM_MIDDLE_PIN = 19;
+const int BOTTOM_RIGHT_PIN = 17;
+
+int pins[9] = {TOP_LEFT_PIN, TOP_MIDDLE_PIN, TOP_RIGHT_PIN, MIDDLE_LEFT_PIN, MIDDLE_MIDDLE_PIN, MIDDLE_RIGHT_PIN, BOTTOM_LEFT_PIN, BOTTOM_MIDDLE_PIN, BOTTOM_RIGHT_PIN};
 const int keyCount = sizeof(pins) / sizeof(pins[0]);
 class Key {
 public:
@@ -19,7 +34,6 @@ void setup() {
   }
 
   for (int i = 0; i < keyCount; i++) {
-    keys[i].pin = pins[i];
     keys[i].isFilledRect = false;
   }
   tft.initR(INITR_BLACKTAB);
@@ -27,92 +41,85 @@ void setup() {
   tft.setCursor(0, 0);
   tft.fillScreen(ST7735_BLACK);
   tft.setTextColor(ST7735_WHITE, ST7735_BLACK);
-
   tft.drawRect(24, 8, 32, 32, 0xFFFF);
-
   tft.drawRect(64, 8, 32, 32, 0xFFFF);
-
   tft.drawRect(104, 8, 32, 32, 0xFFFF);
-
   tft.drawRect(24, 48, 32, 32, 0xFFFF);
-
   tft.drawRect(65, 48, 32, 32, 0xFFFF);
-
   tft.drawRect(104, 48, 32, 32, 0xFFFF);
-
   tft.drawRect(24, 88, 32, 32, 0xFFFF);
-
   tft.drawRect(64, 88, 32, 32, 0xFFFF);
-
   tft.drawRect(104, 88, 32, 32, 0xFFFF);
 }
 void loop() {
-
-  Serial.println();
-  analogRead(28);
-  delay(10);
+  Keyboard.releaseAll();
+  delay(50);
   for (int pin : pins) {
     if (digitalRead(pin) == LOW) {
       switch (pin) {
-      case 9:
-        Keyboard.write('a');
+      case TOP_LEFT_PIN:
+        Keyboard.press('a');
+        // if you want to use a modifier key or multiple of them, follow the example below
+        // Keyboard.press(KEY_LEFT_CTRL);
+        // Keyboard.press(KEY_LEFT_ALT);
+        // Keyboard.press(KEY_DELETE);
         if (keys[0].isFilledRect == false) {
           tft.fillRect(24, 8, 32, 32, 0xFFFF);
           keys[0].isFilledRect = true;
         }
         break;
-      case 10:
-        Keyboard.write('b');
+      case TOP_MIDDLE_PIN:
+        Keyboard.press('b');
         if (keys[1].isFilledRect == false) {
-          tft.fillRect(24, 48, 32, 32, 0xFFFF);
+          tft.fillRect(64, 8, 32, 32, 0xFFFF);
           keys[1].isFilledRect = true;
         }
         break;
-      case 12:
-        Keyboard.write('c');
+      case TOP_RIGHT_PIN:
+        Keyboard.press('c');
         if (keys[2].isFilledRect == false) {
-          tft.fillRect(24, 88, 32, 32, 0xFFFF);
+          tft.fillRect(104, 8, 32, 32, 0xFFFF);
           keys[2].isFilledRect = true;
         }
         break;
-      case 15:
-        Keyboard.write('d');
+      case MIDDLE_LEFT_PIN:
+        Keyboard.press('d');
         if (keys[3].isFilledRect == false) {
-          tft.fillRect(64, 8, 32, 32, 0xFFFF);
+          tft.fillRect(24, 48, 32, 32, 0xFFFF);
 
           keys[3].isFilledRect = true;
         }
         break;
-      case 11:
-        Keyboard.write('e');
+      case MIDDLE_MIDDLE_PIN:
+        Keyboard.press('e');
         if (keys[4].isFilledRect == false) {
           tft.fillRect(65, 48, 32, 32, 0xFFFF);
           keys[4].isFilledRect = true;
         }
         break;
-      case 19:
-        Keyboard.write('f');
+      case MIDDLE_RIGHT_PIN:
+        Keyboard.press('f');
         if (keys[5].isFilledRect == false) {
-          tft.fillRect(64, 88, 32, 32, 0xFFFF);
+          tft.fillRect(104, 48, 32, 32, 0xFFFF);
           keys[5].isFilledRect = true;
         }
         break;
-      case 16:
-        Keyboard.write('g');
+      case BOTTOM_LEFT_PIN:
+        Keyboard.press('g');
         if (keys[6].isFilledRect == false) {
-          tft.fillRect(104, 8, 32, 32, 0xFFFF);
+          tft.fillRect(24, 88, 32, 32, 0xFFFF);
           keys[6].isFilledRect = true;
         }
         break;
-      case 21:
-        Keyboard.write('h');
+      case BOTTOM_MIDDLE_PIN:
+        Keyboard.press('h');
         if (keys[7].isFilledRect == false) {
-          tft.fillRect(104, 48, 32, 32, 0xFFFF);
+          tft.fillRect(64, 88, 32, 32, 0xFFFF);
           keys[7].isFilledRect = true;
         }
         break;
-      case 17:
-        Keyboard.write('i');
+      case BOTTOM_RIGHT_PIN:
+        Keyboard.press('i');
         if (keys[8].isFilledRect == false) {
           tft.fillRect(104, 88, 32, 32, 0xFFFF);
           keys[8].isFilledRect = true;
@@ -121,63 +128,63 @@ void loop() {
       }
     } else {
       switch (pin) {
-      case 9:
+      case TOP_LEFT_PIN:
         if (keys[0].isFilledRect == true) {
           tft.fillRect(24, 8, 32, 32, 0x0000);
           tft.drawRect(24, 8, 32, 32, 0xFFFF);
         }
         keys[0].isFilledRect = false;
         break;
-      case 10:
+      case TOP_MIDDLE_PIN:
         if (keys[1].isFilledRect == true) {
-          tft.fillRect(24, 48, 32, 32, 0x0000);
-          tft.drawRect(24, 48, 32, 32, 0xFFFF);
-        }
-        keys[1].isFilledRect = false;
-        break;
-      case 12:
-        if (keys[2].isFilledRect == true) {
-          tft.fillRect(24, 88, 32, 32, 0x0000);
-          tft.drawRect(24, 88, 32, 32, 0xFFFF);
-        }
-        keys[2].isFilledRect = false;
-        break;
-      case 15:
-        if (keys[3].isFilledRect == true) {
           tft.fillRect(64, 8, 32, 32, 0x0000);
           tft.drawRect(64, 8, 32, 32, 0xFFFF);
         }
+        keys[1].isFilledRect = false;
+        break;
+      case TOP_RIGHT_PIN:
+        if (keys[2].isFilledRect == true) {
+          tft.fillRect(104, 8, 32, 32, 0x0000);
+          tft.drawRect(104, 8, 32, 32, 0xFFFF);
+        }
+        keys[2].isFilledRect = false;
+        break;
+      case MIDDLE_LEFT_PIN:
+        if (keys[3].isFilledRect == true) {
+          tft.fillRect(24, 48, 32, 32, 0x0000);
+          tft.drawRect(24, 48, 32, 32, 0xFFFF);
+        }
         keys[3].isFilledRect = false;
         break;
-      case 11:
+      case MIDDLE_MIDDLE_PIN:
         if (keys[4].isFilledRect == true) {
           tft.fillRect(65, 48, 32, 32, 0x0000);
           tft.drawRect(65, 48, 32, 32, 0xFFFF);
         }
         keys[4].isFilledRect = false;
         break;
-      case 19:
+      case MIDDLE_RIGHT_PIN:
         if (keys[5].isFilledRect == true) {
-          tft.fillRect(64, 88, 32, 32, 0x0000);
-          tft.drawRect(64, 88, 32, 32, 0xFFFF);
-        }
-        keys[5].isFilledRect = false;
-        break;
-      case 16:
-        if (keys[6].isFilledRect == true) {
-          tft.fillRect(104, 8, 32, 32, 0x0000);
-          tft.drawRect(104, 8, 32, 32, 0xFFFF);
-        }
-        keys[6].isFilledRect = false;
-        break;
-      case 21:
-        if (keys[7].isFilledRect == true) {
           tft.fillRect(104, 48, 32, 32, 0x0000);
           tft.drawRect(104, 48, 32, 32, 0xFFFF);
         }
+        keys[5].isFilledRect = false;
+        break;
+      case BOTTOM_LEFT_PIN:
+        if (keys[6].isFilledRect == true) {
+          tft.fillRect(24, 88, 32, 32, 0x0000);
+          tft.drawRect(24, 88, 32, 32, 0xFFFF);
+        }
+        keys[6].isFilledRect = false;
+        break;
+      case BOTTOM_MIDDLE_PIN:
+        if (keys[7].isFilledRect == true) {
+          tft.fillRect(64, 88, 32, 32, 0x0000);
+          tft.drawRect(64, 88, 32, 32, 0xFFFF);
+        }
         keys[7].isFilledRect = false;
         break;
-      case 17:
+      case BOTTOM_RIGHT_PIN:
         if (keys[8].isFilledRect == true) {
           tft.fillRect(104, 88, 32, 32, 0x0000);
           tft.drawRect(104, 88, 32, 32, 0xFFFF);
